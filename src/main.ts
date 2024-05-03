3615d6ff19e65dfa5a8dd2d40a98b978787a15e2
DeTuksa <emmydave414@gmail.com>
2024-05-03 15:44:56 +0100

    Added dependencies and initialized prisma

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { json, urlencoded } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true
+  }))
+  const config = new DocumentBuilder()
+    .setTitle('LC-Airline API')
+    .setDescription('The LC-Airline API documentation')
+    .setVersion('1.0')
+    .addOAuth2()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+  app.use(json({ limit: '50mb' }));
+  app.enableCors()
+  app.use(urlencoded({ extended: true, limit: '50mb' }));
+  await app.listen(3000, '0.0.0.0', () => {
+    console.log(`Server is running on http://0.0.0.0:3000`);
+  });
 }
 bootstrap();
